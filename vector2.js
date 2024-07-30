89a3c6f1617fd65bacaf86b050efa1a02afbf903
ZeroByter <romgamesrom@gmail.com>
2024-07-30 22:30:12 +0300

    updated vector2 class

--- vector2.js
+++ vector2.js
@@ -9,7 +9,7 @@ export default class vector2 {
     }
   }
 
-  add = (x, y) => {
+  add(x, y) {
     if (y == null && x != null) {
       y = x.y;
       x = x.x;
@@ -17,7 +17,7 @@ export default class vector2 {
 
     return new vector2(this.x + x, this.y + y);
   };
-  minus = (x, y) => {
+  minus(x, y) {
     if (y == null && x != null) {
       y = x.y;
       x = x.x;
@@ -25,41 +25,41 @@ export default class vector2 {
 
     return new vector2(this.x - x, this.y - y);
   };
-  multiply = (factor) => {
+  multiply(factor) {
     return new vector2(this.x * factor, this.y * factor);
   };
-  divide = (factor) => {
+  divide(factor) {
     return new vector2(this.x / factor, this.y / factor);
   };
-  floor = () => {
+  floor() {
     return new vector2(Math.floor(this.x), Math.floor(this.y));
   };
-  ceil = () => {
+  ceil() {
     return new vector2(Math.ceil(this.x), Math.ceil(this.y));
   };
-  round = () => {
+  round() {
     return new vector2(Math.round(this.x), Math.round(this.y));
   };
-  magnitude = () => {
+  magnitude() {
     return Math.sqrt(this.x * this.x + this.y * this.y);
   };
-  normalized = () => {
+  normalized() {
     const length = this.magnitude();
     return new vector2(this.x / length, this.y / length);
   };
-  static distance = (x1, y1, x2, y2) => {
+  static distance(x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
   };
-  distance = (otherVector) => {
+  distance(otherVector) {
     return vector2.distance(this.x, this.y, otherVector.x, otherVector.y);
   };
-  midWayTo = (otherVector) => {
+  midWayTo(otherVector) {
     return new vector2(
       (this.x + otherVector.x) / 2,
       (this.y + otherVector.y) / 2
     );
   };
-  toAngle = () => {
+  toAngle() {
     let angle = (Math.atan2(this.x, this.y) / Math.PI) * 180;
 
     if (angle > 180) angle -= 360;
@@ -67,7 +67,7 @@ export default class vector2 {
 
     return angle;
   };
-  static fromAngle = (angle) => {
+  static fromAngle(angle) {
     if (angle > 180) angle -= 360;
     if (angle < -180) angle += 360;
 
@@ -76,16 +76,16 @@ export default class vector2 {
       Math.cos((angle * Math.PI) / 180)
     );
   };
-  newVectorFromAngleAndDistance = (angle, length) => {
+  newVectorFromAngleAndDistance(angle, length) {
     return new vector2(
       this.x + Math.sin((angle * Math.PI) / 180) * length,
       this.y + Math.cos((angle * Math.PI) / 180) * length
     );
   };
-  clone = () => {
+  clone() {
     return new vector2(this.x, this.y);
   };
-  equals = (otherVector) => {
+  equals(otherVector) {
     return this.x == otherVector.x && this.y == otherVector.y;
   };
 }
